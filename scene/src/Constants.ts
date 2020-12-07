7417c24d7daaf7eed5213925b0c8ee486b90739f
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-12-07 22:02:05 +0000

    deployment

--- scene/src/Constants.ts
+++ scene/src/Constants.ts
@@ -1,6 +1,9 @@
-export const WHITEBOARD_SERVER_HOST = '192.168.1.11';
+// export const WHITEBOARD_SERVER_HOST = 'whiteboard.levalleux.online';
+export const WHITEBOARD_SERVER_HOST = '77.68.4.79';
+// export const WHITEBOARD_SERVER_HOST = '127.0.0.1';
 export const WHITEBOARD_WS = `ws://${WHITEBOARD_SERVER_HOST}:13370`;
-export const WHITEBOARD_HTTP = `http://${WHITEBOARD_SERVER_HOST}:80`;
-export const WHITEBOARD_APP_URL = 'http://192.168.1.11:4200';
+export const WHITEBOARD_HTTP = `http://${WHITEBOARD_SERVER_HOST}:8888`;
+export const WHITEBOARD_APP_URL = 'https://dcl-pictionary.vercel.app';
+// export const WHITEBOARD_APP_URL = 'http://localhost:4200';
 export const PCT_CONTRACT = '0x1B1029C79dcf5aAA973b00eE71BcB7184EfC02F5';
 export const NFT_CONTRACT = '0x1F1409978E4c1D9Cc6c7A8F1513F924d15bf2b41';
--- whiteboard-app/src/environments/environment.prod.ts
+++ whiteboard-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  ws_url: 'ws://pictionary.levalleux.online:13370',
-  http_url: 'http://pictionary.levalleux.online:8888'
+  ws_url: 'ws://77.68.4.79:13370',
+  http_url: 'http://77.68.4.79:8888'
 };
