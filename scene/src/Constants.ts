62414d81e44d5a5bdfba2e9b82c89f97daa85cb2
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-12-20 20:27:01 +0000

    deploy v2 (without wss)

--- scene/src/Constants.ts
+++ scene/src/Constants.ts
@@ -1,15 +1,15 @@
-// export const WHITEBOARD_SERVER_HOST = 'pictionary.levalleux.online';
+export const WHITEBOARD_SERVER_HOST = 'pictionary.levalleux.online';
 // export const WHITEBOARD_SERVER_HOST = '77.68.4.79';
-export const WHITEBOARD_SERVER_HOST = '192.168.1.11';
+// export const WHITEBOARD_SERVER_HOST = '192.168.1.11';
 export const WHITEBOARD_WS = `ws://${WHITEBOARD_SERVER_HOST}:13370`;
 export const WHITEBOARD_HTTP = `http://${WHITEBOARD_SERVER_HOST}:8888`;
-// export const WHITEBOARD_APP_URL = 'http://pictionary.levalleux.online:8887';
-export const WHITEBOARD_APP_URL = 'http://localhost:4200';
+export const WHITEBOARD_APP_URL = 'http://pictionary.levalleux.online:8887';
+// export const WHITEBOARD_APP_URL = 'http://localhost:4200';
 // export const PCT_CONTRACT = '0x1B1029C79dcf5aAA973b00eE71BcB7184EfC02F5';
 // export const NFT_CONTRACT = '0x1F1409978E4c1D9Cc6c7A8F1513F924d15bf2b41';
-export const PCT_CONTRACT = '0xf0D06B335fcDcF59eb602DeECBc4Dd54A64eBF8b';
-export const NFT_CONTRACT = '0x23699D88166B5b5CC4F3a9D1c2fF6D3D59127a52';
+export const PCT_CONTRACT = '0x875F95a42F058Ba21C3d33Bcc184949238643060';
+export const NFT_CONTRACT = '0x3818a45ca7754Ddbf47fB2aA39F34517C8ccF0a7';
 export const L2_NETWORK = {
-    rpcNode: 'http://192.168.1.11:7545',
-    chainId: 1337
+    rpcNode: 'https://rpc-mumbai.maticvigil.com',
+    chainId: 80001
 };
--- whiteboard-server/src/config.ts
+++ whiteboard-server/src/config.ts
@@ -14,22 +14,22 @@ if (!process.env.MNEMONIC) {
   
 export const WHITEBOARD_APP_URL = process.env.APP_URL;
 export const IMAGE_STORE = process.env.STORE + '/NFT/images';
-export const IMAGE_SERVER_URL = 'E:/temp/NFT/images';
+export const IMAGE_SERVER_URL = 'https://pictionary.levalleux.online/NFT/images';
 export const METADATA_STORE = process.env.STORE + '/NFT';
-export const METADATA_SERVER_URL = 'E:/temp/NFT';
-// export const WEB3_NETWORK = {
-//     chainId: 80001,
-//     name: 'Mumbai',
-//     nodeUrl: 'https://rpc-mumbai.maticvigil.com'
-// };
-// export const PCT_CONTRACT = '0x1B1029C79dcf5aAA973b00eE71BcB7184EfC02F5';
-// export const NFT_CONTRACT = '0x1F1409978E4c1D9Cc6c7A8F1513F924d15bf2b41';
-
+export const METADATA_SERVER_URL = 'https://pictionary.levalleux.online/NFT';
 export const WEB3_NETWORK = {
-    chainId: 1337,
-    name: 'Ganache',
-    nodeUrl: 'http://192.168.1.11:7545'
+    chainId: 80001,
+    name: 'Mumbai',
+    nodeUrl: 'https://rpc-mumbai.maticvigil.com'
 };
-export const PCT_CONTRACT = '0xf0D06B335fcDcF59eb602DeECBc4Dd54A64eBF8b';
-export const NFT_CONTRACT = '0x23699D88166B5b5CC4F3a9D1c2fF6D3D59127a52';
+export const PCT_CONTRACT = '0x875F95a42F058Ba21C3d33Bcc184949238643060';
+export const NFT_CONTRACT = '0x3818a45ca7754Ddbf47fB2aA39F34517C8ccF0a7';
+
+// export const WEB3_NETWORK = {
+//     chainId: 1337,
+//     name: 'Ganache',
+//     nodeUrl: 'http://192.168.1.11:7545'
+// };
+// export const PCT_CONTRACT = '0xf0D06B335fcDcF59eb602DeECBc4Dd54A64eBF8b';
+// export const NFT_CONTRACT = '0x23699D88166B5b5CC4F3a9D1c2fF6D3D59127a52';
 export const GUESSING_TIMEOUT = 45000;
